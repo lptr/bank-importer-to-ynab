1520f623dd99175c6be3d043e9143c4bb370bcbd
Lóránt Pintér <lorant.pinter@gmail.com>
2021-10-17 17:55:13 +0200

    Add file upload

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <wise-importer/>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
-import HelloWorld from './components/HelloWorld.vue';
+import WiseImporter from './components/WiseImporter.vue';
 
 @Options({
   components: {
-    HelloWorld,
+    WiseImporter,
   },
 })
 export default class App extends Vue {}
@@ -17,11 +16,9 @@ export default class App extends Vue {}
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/WiseImporter.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h1>Wise importer</h1>
+    <input type="file" @change="onFileChange" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+  props: {
+  },
+  methods: {
+    onFileChange(event: any) {
+      const file = event.target.files[0];
+      console.log(file);
+      // const reader = new FileReader();
+      // reader.onload = (e: any) => {
+      //   console.log(e.target.result);
+      // };
+    },
+  },
+})
+export default class HelloWorld extends Vue {
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
